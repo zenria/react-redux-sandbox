6bc5f43e2a25bc8523ba819cdbe676d0fbb7f595
Philippe GASSMANN <philoops@gmail.com>
2016-11-26 23:00:26 +0100

    boilerplate in reducers.js

--- src/redux-app/ReduxApp.js
+++ src/redux-app/ReduxApp.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { Provider,connect } from 'react-redux';
-import createLogger from 'redux-logger';
-import { applyMiddleware, createStore } from 'redux';
-import {reducers} from "./reducers";
-import createSagaMiddleware from 'redux-saga'
-import {rootSaga} from './sagas'
+import {createAndInitStore} from "./reducers";
 import Checkbox from './Checkbox'
 import DisplayIfChecked from './DisplayIfChecked'
 import LoadButton from './LoadButton'
@@ -13,15 +9,9 @@ import DataDisplayer from './DataDisplayer'
 class ReduxApp extends Component {
     constructor(props){
         super(props);
-        const sagaMiddleware = createSagaMiddleware();
         this.state = {
-            store:  createStore(
-                reducers,
-                {checkboxChecked: false, somethingConstant: "This will never change."},
-                applyMiddleware(createLogger(), sagaMiddleware)
-            )
+            store:  createAndInitStore()
         }
-        sagaMiddleware.run(rootSaga);
     }
 
   render() {
--- src/redux-app/reducers.js
+++ src/redux-app/reducers.js
@@ -1,10 +1,14 @@
 import { handleAction } from 'redux-actions';
-import {combineReducers} from 'redux';
+import {combineReducers, applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga'
+import {rootSaga} from './sagas'
+import createLogger from 'redux-logger';
 import {
     SET_CHECKBOX,
     SHOW_LOADING,
     DATA_LOADED
 } from './actions.js'
+
 type State = {
     checkboxChecked: boolean
 }
@@ -21,9 +25,19 @@ const showLoading = createPayloadIdendityReducer(SHOW_LOADING, false);
 
 const data = createPayloadIdendityReducer(DATA_LOADED, null);
 
-export const reducers = combineReducers({
+const reducers = combineReducers({
     checkboxChecked,
     showLoading,
     data,
     somethingConstant: identityReducer
 })
+
+export const createAndInitStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
+    const store = createStore(reducers, {
+        checkboxChecked: false,
+        somethingConstant: "This will never change."
+    }, applyMiddleware(createLogger(), sagaMiddleware));
+    sagaMiddleware.run(rootSaga);
+    return store;
+}
